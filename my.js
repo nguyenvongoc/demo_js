244838f10ef0c6084d5dd7b374cd35a1e343ba9b
nguyenvongoc <65283435+nguyenvongoc@users.noreply.github.com>
2021-05-13 20:32:09 +0700

    demo js

--- my.js
+++ my.js
@@ -2,10 +2,14 @@ let numberOne= +prompt("Nhap so thu 1: ");
 let numberTwo= +prompt("Nhap so thu 1: ");
 if(numberOne==0)
 {
-    if(numberTwo==0) print("Phuong trinh co o so nghiem")
-    else print("Phuong trinh vo nghiem")
-
+    if(numberTwo==0)
+    {
+         alert("Phuong trinh co vo so nghiem");    
+    } else { 
+        alert("Phuong trinh vo nghiem");
+            }
 }
-else x=-numberTwo/numberOne;
-alert(x);
-    
+else {
+    x=-numberTwo/numberOne;
+alert("Phuong trinh co nghiem :"+x);
+}
